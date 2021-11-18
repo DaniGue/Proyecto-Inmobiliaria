75f9533b66a03507651e6966b27068c7398cdbbc
Fredy Alejandro Camelo Guerrero <fredytoh@gmail.com>
2021-11-18 01:10:59 -0500

    se sube ajuste de guardado en bases de datos

---
+++ appServer.js
@@ -0,0 +1,88 @@
+express = require("express");
+const { createReadStream } = require('fs')
+
+const app = new express();
+//const router = new express.Router();
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+//Conecta a base de datos mongodb
+require('./conexion');
+//Crea los modelos a utilizar para las operaciones crud
+var modeloInmueble = require('./Modelos/inmuebles');
+var modeloUsuario = require('./Modelos/usuarios');
+
+const path = require("path");
+let fs = require('fs')
+const multer = require("multer");
+const { monitorEventLoopDelay } = require("perf_hooks");
+let objMulter = multer({ dest: "./public/upload" });
+// Instantiate multer, el objeto de parámetro pasado, dest representa la ruta de almacenamiento del archivo cargado
+app.use(objMulter.any())
+
+app.use(express.static("./public"));
+
+//#region  Operaciones CRUD
+
+//#region INMUEBLE
+app.post("/insertarInmueble", (req, res) => {
+    var myobj = {
+        nombre: req.body.nombre,
+        tipo: 'defecto',
+        imagen: 'defecto',
+        ubicacion: 'Bogota'
+    };
+    modeloInmueble.collection.insertOne(myobj, function (err, res) {
+        if (err) {
+            throw err;
+        }
+        console.log('1 inmueble insertado');
+    });
+});
+//#endregion INMUEBLE
+//#region USUARIO
+app.post("/insertarUsuario", (req, res) => {
+    var usuario  = new modeloUsuario({
+        cedula: req.body.cedula,
+        nombre: req.body.nombre,
+        apellido: req.body.apellido,
+        email: req.body.email,
+        contrasena: req.body.contrasena
+    });
+    usuario.save(function(err,datos){
+        if (err) {
+            console.error(err);
+            throw err;
+        }
+        res.redirect('./Login');
+    });
+});
+//consultar usuario
+app.post("/consultarUsuario", (req, res) => {
+    modeloUsuario.findOne({ email: req.body.email, contrasena: req.body.contrasena },function(err,datos){
+        if (err) {
+            console.error(err);
+            throw err;
+        }else{
+            if(datos){
+                res.send(datos);
+            }else{
+                res.send("Usuario no encontrado");
+            }
+            
+        }
+          
+    }) 
+});
+//#endregion USUARIO
+
+//#endregion  Operaciones CRUD
+
+//Agregar las rutas de las operaciones crud al servidor
+//app.use(router);
+//Iniciar el servidor en el puerto designado
+app.listen(5000, () => {
+
+    console.log("aplicacion corriendo en el puerto 5000")
+
+})
---
+++ conexion.js
@@ -0,0 +1,10 @@
+var mongoose = require('mongoose');
+//const monngodb_url='mongodb://localhost:27017/Inmobiliaria?readPreference=primary&appname=MongoDB%20Compass&ssl=false';
+const monngodb_url = 'mongodb://mongodbuser:mongodb_123456789@dbclase4a-shard-00-00.cvnpc.mongodb.net:27017,dbclase4a-shard-00-01.cvnpc.mongodb.net:27017,dbclase4a-shard-00-02.cvnpc.mongodb.net:27017/Inmobiliaria?ssl=true&replicaSet=atlas-dlxizx-shard-0&authSource=admin&retryWrites=true&w=majority';
+
+mongoose.connect(monngodb_url, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+    console.log("mongodb esta conectado");
+}).catch((error) => {
+    console.log("mongodb no esta conectado");
+    console.log(error);
+});
