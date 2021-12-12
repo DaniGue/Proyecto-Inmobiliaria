3c19075acb5d9df2abd5e0fcfe7a0c9586dbd144
heiiit <titav5261@gmail.com>
2021-12-12 14:28:42 -0500

    intento de consultar ubicacio

--- AppInmobiliariaVista/src/app/ubicacion/consultar/consultar.component.ts
+++ AppInmobiliariaVista/src/app/ubicacion/consultar/consultar.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { UbicacionService } from 'src/app/service/ubicacion/ubicacion.service';
 
 @Component({
   selector: 'app-consultar',
   templateUrl: './consultar.component.html',
   styleUrls: ['./consultar.component.css']
 })
-export class ConsultarUbicacionComponent implements OnInit {
+export class ConsultarUbicacionComponent implements OnInit{
+  ubicacion: any;
+  listaUbicaciones: any;
+  constructor(private ubicacion_Service: UbicacionService, private http: HttpClient) {
 
-  constructor() { }
-
-  ngOnInit(): void {
   }
 
+  ngOnInit(): void {
+    this.ubicacion_Service.consultarUbicacion({}).subscribe((data: any) => {
+      this.listaUbicaciones = data.datos;
+      console.log(data);
+    });
+  
+  }
+
+  consultarUbicacionFiltro() {
+    let ciudad = (document.getElementById("ciudad") as HTMLInputElement).value;
+    let departamento = (document.getElementById("departamento") as HTMLInputElement).value;
+  
+    //tomo los datos y se los envío al servicio
+    this.ubicacion = {
+      ciudad: ciudad,
+      departamento: departamento,
+    };
+    this.ubicacion_Service.consultarUbicacion(this.ubicacion).subscribe((data: any) => {
+      this.listaUbicaciones = data.datos;
+      console.log(data);
+    })
+  }
+  
+  reloadCurrentPage() {
+    window.location.reload();
+  }
 }
